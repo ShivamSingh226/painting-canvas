9007fef7a533cf8241381085fbee0fbb090904f2
ShivamSingh226 <shivamsingh226@gmail.com>
2023-12-05 15:22:41 +0530

    Added slice and reducer for MenuBar

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,15 +1,23 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-
+import cx from 'classnames'
 
 import { faPencil,faEraser,faRotateLeft,faRotateRight,faFileArrowDown } from "@fortawesome/free-solid-svg-icons"
 import styles from "./index.module.css"
+import { useDispatch, useSelector } from "react-redux"
+import { MENU_ITEMS } from "@/constants"
+import { menuItemClick,actionItemClick } from "@/slice/menuSlice"
 const Menu=()=>{
+    const dispatch=useDispatch();
+    const activeMenuItem=useSelector((state)=>state.menu.activeMenuItem);
+    const handleMenuClick=(item)=>{
+        dispatch(menuItemClick(item))
+    }
   return( 
   <div className={styles.menuContainer}>
-        <div className={styles.iconWrapper}>
-            <FontAwesomeIcon icon={faPencil} className={styles.icon}/>
+        <div className={cx(styles.iconWrapper,{[styles.active]:activeMenuItem===MENU_ITEMS.PENCIL})} onClick={()=>handleMenuClick(MENU_ITEMS.PENCIL)}>
+            <FontAwesomeIcon icon={faPencil} className={styles.icon} />
         </div>
-        <div className={styles.iconWrapper}>
+        <div className={cx(styles.iconWrapper,{[styles.active]:activeMenuItem===MENU_ITEMS.ERASER})}  onClick={()=>handleMenuClick(MENU_ITEMS.ERASER)}>
             <FontAwesomeIcon icon={faEraser} className={styles.icon}/>
         </div>
         <div className={styles.iconWrapper}>
--- src/components/Toolbox/index.js
+++ src/components/Toolbox/index.js
@@ -1,13 +1,18 @@
+import { useSelector } from "react-redux"
 import styles from "./index.module.css"
-import { COLORS } from "@/constants"
+import { COLORS,MENU_ITEMS } from "@/constants"
 const Toolbox=()=>{
+    const activeMenuItem=useSelector((state)=>state.menu.activeMenuItem);
+    const showStrokeToolOption=activeMenuItem===MENU_ITEMS.PENCIL;
+    const showBrushToolOption=activeMenuItem===MENU_ITEMS.PENCIL|| activeMenuItem===MENU_ITEMS.ERASER;
+
     const updateBrushSize=(e)=>{
 
 
     }
     return(
         <div className={styles.toolboxContainer}>
-            <div className={styles.toolItem}>
+            {showStrokeToolOption && <div className={styles.toolItem}>
                 <h4 className={styles.toolText}>Stroke color</h4>
                 <div className={styles.itemContainer}>
                     <div className={styles.colorBox} style={{backgroundColor:COLORS.BLACK}}/>
@@ -17,15 +22,17 @@ const Toolbox=()=>{
                     <div className={styles.colorBox} style={{backgroundColor:COLORS.YELLOW}}/>
                     <div className={styles.colorBox} style={{backgroundColor:COLORS.ORANGE}}/>
                 </div>
-            </div>
-            <div className={styles.toolItem}>
+            </div>}
+            {showBrushToolOption&&   <div className={styles.toolItem}>
                 <h4 className={styles.toolText}>Brush Size</h4>
                 <div className={styles.itemContainer}>
                     <input type="range" min={1} max={10} step={1} onChange={updateBrushSize}/>
 
                     
                 </div>
-            </div>
+            </div>}
+            
+          
         </div>
 
     )
